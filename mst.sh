5cc529959a079a578a5482feabc9b8361382657e
agile6v <agile6v@agile6v.com>
2017-03-01 20:51:25 +0800

    support for replace command

--- mst.sh
+++ mst.sh
@@ -20,8 +20,8 @@ usage() {
     echo "    -h, --help       Print help"
     echo
     echo "Commands:"
-    echo "    replace       Replace content in the files"
-    echo "    json          Json pretty print (The input should be inside the single quotes)"
+    echo "    replace       Replace the contents in the files"
+    echo "    json          Json pretty print (The json string should be inside the single quotes)"
     echo
     echo "Use "$SCRIPT_NAME [command] --help" for more information about a command."
     echo
@@ -31,6 +31,7 @@ version() {
     echo "$SCRIPT_NAME v$VERSION"
 }
 
+### json pretty print
 json() {
     check_cmd python;
 
@@ -60,10 +61,55 @@ json_usage() {
     echo
 }
 
+### replace the content in the files
 replace() {
+    if [ $# -eq 0 ];then
+        replace_usage; exit
+    fi
 
-    echo "replace running";
-    #grep -irl "hello" . | xargs sed -i ".bak" 's/hello/hi/g'
+    local DIR="."
+    local FROM=""
+    local TO=""
+
+    while getopts :hd:f:t: opt; do
+        case $opt in
+            d) DIR=$OPTARG; ;;
+            f) FROM=$OPTARG; ;;
+            t) TO=$OPTARG; ;;
+            h) replace_usage; exit ;;
+        esac
+    done
+
+    if [ "$FROM" = "" -o "$TO" = "" ]; then
+        replace_usage; exit;
+    fi
+
+    local SCRIPT="grep -irl $FROM $DIR"
+    local FILES=`eval $SCRIPT`
+    local PATTERN="s/$FROM/$TO/g"
+    if [ "${FILES}" = "" ]; then
+        echo "No occurrences of \"$FROM\" found in the $DIR directory."; exit;
+    fi
+    echo "\nThe following files contain the string \"$FROM\":\n$FILES"
+    echo $FILES | xargs sed -i ".bak" $PATTERN
+    echo
+    echo ">>> Replacement done."
+    echo
+    echo "Notice: The modified file is backed up in the file ending with suffix .bak!"
+}
+
+
+replace_usage() {
+    echo "Usage: $SCRIPT_NAME replace option"
+    echo
+    echo "Options:"
+    echo "    -d    Specify a directory"
+    echo "    -f    A string to serach"
+    echo "    -t    A string to replace"
+    echo
+    echo "i.e."
+    echo "    $SCRIPT_NAME replace -d /tmp/ -f \"from\" -t \"to\""
+    echo
 }
 
 check_cmd() {
@@ -81,7 +127,7 @@ if [ -n "$arg"  ]; then
     case $arg in
         -h|--help) usage; exit ;;
         -V|--version) version; exit ;;
-        replace) replace; exit ;;
+        replace) replace $@; exit ;;
         json) json $@; exit ;;
         *) usage; exit ;;
     esac
